84a8192b01539c1217e3c52fbb27ce8aebcbbba6
nito nise <aakazakovv@yandex.ru>
2021-03-04 14:30:38 +0300

    add session factory

---
+++ src/main/java/dev/fun/taskz/data/SessionFactoryMaster.java
@@ -0,0 +1,33 @@
+package dev.fun.taskz.data;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import dev.fun.taskz.entities.Project;
+import dev.fun.taskz.entities.Task;
+import dev.fun.taskz.entities.User;
+
+public final class SessionFactoryMaster {
+	
+	public static final SessionFactory sessionFactory;
+	
+	static {
+		sessionFactory = new Configuration()
+				.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect")
+				.setProperty("hibernate.connection.driver_class", "org.h2.Driver")
+				.setProperty("hibernate.connection.url", "jdbc:h2:mem:test")
+				.setProperty("hibernate.connection.username", "sa")
+				.setProperty("hibernate.connection.password", "")
+				.setProperty("hibernate.show_sql", "true")
+				.setProperty("hibernate.hbm2ddl.auto", "create-drop")
+				.addAnnotatedClass(User.class)
+				.addAnnotatedClass(Project.class)
+				.addAnnotatedClass(Task.class)
+				.buildSessionFactory();
+	}
+	
+	private SessionFactoryMaster () {
+		
+	}
+	
+}
