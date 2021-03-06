50d022897c483530c8a4cdabdccf51f91ae6bbdd
nito nise <aakazakovv@yandex.ru>
2021-03-06 17:26:24 +0300

    test code

--- src/main/java/dev/fun/taskz/App.java
+++ src/main/java/dev/fun/taskz/App.java
@@ -3,16 +3,20 @@ package dev.fun.taskz;
 import java.io.IOException;
 
 import dev.fun.taskz.data.init.InputDataHandler;
+import dev.fun.taskz.interaction.Terminal;
 
 public class App {
 	
     public static void main( String[] args ) {   	
     	InputDataHandler idh = new InputDataHandler("init_db.txt");
+    	
     	try {
-				idh.launch();
+				idh.initDb();
 			} catch (IOException e) {
 				e.printStackTrace();
-			}        
+			}
+    	
+    	new Thread(new Terminal()).start();
     }
     
 }
