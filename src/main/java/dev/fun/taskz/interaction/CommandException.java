fad826d59140364056bc12f1a86e8359bae1a86a
nito nise <aakazakovv@yandex.ru>
2021-03-07 17:39:45 +0300

    add serial id

--- src/main/java/dev/fun/taskz/interaction/CommandException.java
+++ src/main/java/dev/fun/taskz/interaction/CommandException.java
@@ -1,7 +1,9 @@
 package dev.fun.taskz.interaction;
 
 public class CommandException extends Exception {
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public CommandException() {
 		super();
 	}
