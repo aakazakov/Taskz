41213992053f4d9d52aa4e4d159504c6ef223865
nito nise <aakazakovv@yandex.ru>
2021-03-07 17:34:43 +0300

    add castom exception

---
+++ src/main/java/dev/fun/taskz/interaction/CommandException.java
@@ -0,0 +1,13 @@
+package dev.fun.taskz.interaction;
+
+public class CommandException extends Exception {
+	
+	public CommandException() {
+		super();
+	}
+	
+	public CommandException(String msg) {
+		super(msg);
+	}
+	
+}
