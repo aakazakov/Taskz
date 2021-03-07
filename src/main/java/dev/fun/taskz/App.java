c625f39e9dd5e285a94ae995e85eab95afa3edb5
nito nise <aakazakovv@yandex.ru>
2021-03-07 19:16:17 +0300

    stopped for now

--- src/main/java/dev/fun/taskz/App.java
+++ src/main/java/dev/fun/taskz/App.java
@@ -22,15 +22,27 @@ public class App {
 }
 
 /*
-	ADD COMMANDS:
+	CREATE COMMANDS:
 	add project <title>
 	add user <name>
 	add task <projectID> <description>
 	
 	DELETE COMMANDS:
+	del project <projectID>
+	del user <userID>
+	del task <taskID>
 	
 	INFO COMMANDS:
+	inf project list
+	inf project <projectID>
+	inf user list
+	inf user <userID>
+	inf task <projectID>
+	inf task <taskID>
+	inf tasks <userID> <projectID>
 	
 	OTHER COMMANDS:
-	
+	set status <taskID> <status>
+	assign user <projectID> <userID>
+	assign task <userID> <taskID>
 */
