22a28440a79ab9e1139b129cf9483d13d19062dc
nito nise <aakazakovv@yandex.ru>
2021-03-09 13:46:27 +0300

    fix comments

--- src/main/java/dev/fun/taskz/App.java
+++ src/main/java/dev/fun/taskz/App.java
@@ -5,6 +5,51 @@ import java.io.IOException;
 import dev.fun.taskz.data.init.InputDataHandler;
 import dev.fun.taskz.interaction.Terminal;
 
+/*
+CREATE COMMANDS:
+create project <title>
+create user <name>
+create task <projectID> <description>
+create subtask <parentTaskID> <description>
+
+DELETE COMMANDS:
+delete project <projectID>
+delete user <userID>
+delete task <taskID>
+
+SHOW COMMANDS:
+show project list
+show project <projectID>
+show user list
+show user <userID>
+show task list <projectID>
+show task <taskID>
+show tasks <userID> <projectID>
+
+OTHER COMMANDS:
+set status <taskID> <status>
+assign user <projectID> <userID>
+assign task <userID> <taskID>
+
+command examples:
+create project AwesomeProject // saved title: AwesomeProject
+create project 'AwesomeProject' // saved title: AwesomeProject
+create project 'AwesomeProject // saved title: AwesomeProject
+create project 'Awesome Project' // saved title: Awesome Project
+create project 'Awesome Project // saved title: Awesome Project
+create project 'Awesome Project bla-bla-bla // saved title: Awesome Project bla-bla-bla
+create project 'Awesome Project' bla-bla-bla // saved title: Awesome Project
+create project Awesome Project bla-bla-bla // saved title: Awesome
+
+show project 2
+
+assign user 2 1
+
+set status 4 done
+
+>>> Have fun <<<
+*/
+
 public class App {
 	
     public static void main( String[] args ) {   	
@@ -20,30 +65,3 @@ public class App {
     }
     
 }
-
-/*
-	CREATE COMMANDS:
-	create project <title>
-	create user <name>
-	create task <projectID> <description>
-	create subtask <parentTaskID> <description>
-	
-	DELETE COMMANDS:
-	delete project <projectID>
-	delete user <userID>
-	delete task <taskID>
-	
-	SHOW COMMANDS:
-	show project list
-	show project <projectID>
-	show user list
-	show user <userID>
-	show task list <projectID>
-	show task <taskID>
-	show tasks <userID> <projectID>
-	
-	OTHER COMMANDS:
-	set status <taskID> <status>
-	assign user <projectID> <userID>
-	assign task <userID> <taskID>
-*/
