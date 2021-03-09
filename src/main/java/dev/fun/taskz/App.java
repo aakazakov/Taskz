ec7435bf1cca8373bd2571598ce508af842f5dca
nito nise <aakazakovv@yandex.ru>
2021-03-09 14:08:07 +0300

    makeup

--- src/main/java/dev/fun/taskz/App.java
+++ src/main/java/dev/fun/taskz/App.java
@@ -9,8 +9,8 @@ import dev.fun.taskz.interaction.Terminal;
 CREATE COMMANDS:
 create project <title>
 create user <name>
-create task <projectID> <description>
-create subtask <parentTaskID> <description>
+create task <projectID> <content> // creates a task in the specified project (<content> means task description)
+create subtask <parentTaskID> <content> // creates a subtask for the specified task
 
 DELETE COMMANDS:
 delete project <projectID>
@@ -24,14 +24,14 @@ show user list
 show user <userID>
 show task list <projectID>
 show task <taskID>
-show tasks <userID> <projectID>
+show tasks <userID> <projectID> // displays a list of user tasks in the specified project
 
 OTHER COMMANDS:
-set status <taskID> <status>
-assign user <projectID> <userID>
-assign task <userID> <taskID>
+set status <taskID> <status> // sets the actual status of the task
+assign user <projectID> <userID> // assigns user on the project
+assign task <userID> <taskID> // assigns task on the user
 
-command examples:
+EXAMPLES:
 create project AwesomeProject // saved title: AwesomeProject
 create project 'AwesomeProject' // saved title: AwesomeProject
 create project 'AwesomeProject // saved title: AwesomeProject
@@ -40,12 +40,10 @@ create project 'Awesome Project // saved title: Awesome Project
 create project 'Awesome Project bla-bla-bla // saved title: Awesome Project bla-bla-bla
 create project 'Awesome Project' bla-bla-bla // saved title: Awesome Project
 create project Awesome Project bla-bla-bla // saved title: Awesome
-
 show project 2
-
 assign user 2 1
-
 set status 4 done
+create task 2 'Greet the world' // in the project with id = 2 a task with the content "Greet the world" will be created
 
 >>> Have fun <<<
 */
